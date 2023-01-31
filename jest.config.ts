930a2729b845b3dc2b1a9d91f724d8e730d28ad4
matheusdearaujo <matheusouzadearaujo@gmail.com>
2023-01-31 09:22:59 -0300

    initial commit

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import { Config } from "jest";
+
+const config: Config = {
+  moduleFileExtensions: ["js", "json", "ts"],
+  testRegex: ".*\\.spec\\.ts$",
+  transform: {
+    "^.+\\.(t|j)s$": "ts-jest",
+  },
+
+  collectCoverageFrom: ["**/*.(t|j)s"],
+  coverageDirectory: "../coverage",
+  testEnvironment: "node",
+
+  moduleNameMapper: {
+    "@/(.*)": "<rootDir>/src/$1",
+  },
+};
+
+export default config;
