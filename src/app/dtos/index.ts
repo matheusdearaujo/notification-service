dd9ea8ad597b0539ee2fe1416f3cfab9c79f3e10
matheusdearaujo <matheusouzadearaujo@gmail.com>
2023-01-31 23:30:50 -0300

    feat: add dtos

--- src/app/dtos/index.ts
+++ src/app/dtos/index.ts
@@ -1,3 +1,7 @@
 export * from "./send-notification-request.dto";
 export * from "./send-notification-response.dto";
 export * from "./cancel-notification-request.dto";
+export * from "./count-recipient-notifications-request.dto";
+export * from "./count-recipient-notifications-response.dto";
+export * from "./get-recipient-notifications-request.dto";
+export * from "./get-recipient-notifications-response.dto";
